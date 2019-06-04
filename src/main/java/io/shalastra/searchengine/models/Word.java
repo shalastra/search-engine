f04bf6cd24620ca023ac6834a6fa810e2e546878
Szymon Halastra <szymon.edward.halastra@cern.ch>
2019-06-04 17:18:49 +0200

    Create basic models

---
+++ src/main/java/io/shalastra/searchengine/models/Word.java
@@ -0,0 +1,13 @@
+package io.shalastra.searchengine.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+public class Word {
+
+  @Getter
+  @Setter
+  private String word;
+}

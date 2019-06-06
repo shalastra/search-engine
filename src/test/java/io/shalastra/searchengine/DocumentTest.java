48d86b8788d4cb781263789727e5a630c6177b47
Szymon Halastra <szymon.edward.halastra@cern.ch>
2019-06-06 17:35:35 +0200

    Add @After method for cleaning static field

--- src/test/java/io/shalastra/searchengine/DocumentTest.java
+++ src/test/java/io/shalastra/searchengine/DocumentTest.java
@@ -1,16 +1,27 @@
 package io.shalastra.searchengine;
 
+import java.lang.reflect.Field;
+
 import io.shalastra.searchengine.models.Document;
+import org.junit.After;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class DocumentTest {
 
+  @After
+  public void setUp() throws IllegalAccessException, NoSuchFieldException {
+    Field counterField = Document.class.getDeclaredField("counter");
+    counterField.setAccessible(true);
+
+    counterField.set(null, 1);
+  }
+
   @Test
   public void getDocumentLength_ShouldReturnNumberOfWords() {
     Document document = new Document("the brown fox jumped over the brown dog");
-    int expectedLength = document.splitDocument().size();
+    int expectedLength = document.splitText().size();
 
     int wordsNumber = document.getDocumentLength();
 
